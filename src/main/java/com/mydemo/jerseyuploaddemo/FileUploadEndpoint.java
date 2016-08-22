e5472c781df0112b5e36ad5350fa18c3533aee56
xiefei001 <xiefei001@hotmail.com>
2016-08-22 17:01:57 +0200

    upload upgrade

--- src/main/java/com/mydemo/jerseyuploaddemo/FileUploadEndpoint.java
+++ src/main/java/com/mydemo/jerseyuploaddemo/FileUploadEndpoint.java
@@ -6,6 +6,7 @@ package com.mydemo.jerseyuploaddemo;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import java.io.InputStream;
@@ -20,7 +21,16 @@ import java.io.InputStream;
 @Path("/upload")
 public class FileUploadEndpoint {
 
+  /*
+  <form action="/upload" method="post" enctype="multipart/form-data">
+	<p> Wählen Sie eine Textdatei (*.txt, *.html usw.) von Ihrem Rechner aus:<br>
+    <input name="file" type="file" size="50" accept="text/*"> </p>
+     <p> <input name="filename" type="text"> </p>
+    <input type="submit" value="... und ab geht die Post!" name="submit">
+    </form>
+    */
 	@POST
+	@Consumes({"multipart/form-data"})
 	public String upload(@FormDataParam("file") InputStream fileInputStream,
 		@FormDataParam("filename") String filename) {
 		return "uploaded: " + filename;
